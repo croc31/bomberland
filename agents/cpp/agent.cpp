3eaca9c980033c4bf77af3964e102e4bc26d0911
Clauber Câmara <clauber.camara.704@ufrn.edu.br>
2022-12-05 23:34:30 -0300

    configuração para acessar informação do inimigo

--- agents/cpp/agent.cpp
+++ agents/cpp/agent.cpp
@@ -11,6 +11,9 @@ class Agent {
 private:
   static std::string my_agent_id;
   static std::vector<std::string> my_units;
+  static std::string enemy_agent_id;
+  static std::vector<std::string> enemy_units;
+  static std::vector<std::vector<std::string>> map;
 
   static int tick;
 
@@ -21,6 +24,9 @@ public:
 
 std::string Agent::my_agent_id;
 std::vector<std::string> Agent::my_units;
+std::string Agent::enemy_agent_id;
+std::vector<std::string> Agent::enemy_units;
+std::vector<std::vector<std::string>> Agent::map;
 
 int Agent::tick;
 
@@ -51,18 +57,29 @@ void Agent::on_game_tick(int tick_nr, const json& game_state)
   {
     my_agent_id = game_state["connection"]["agent_id"];
     my_units = game_state["agents"][my_agent_id]["unit_ids"].get<std::vector<std::string>>();
+    enemy_agent_id = (my_agent_id == "a" ? "b" : "a");
+    enemy_units = game_state["agents"][enemy_agent_id]["unit_ids"].get<std::vector<std::string>>();
   }
 
+  //
+
   srand(1234567 * (my_agent_id == "a" ? 1 : 0) + tick * 100 + 13);
+  // srand(1234567 * (enemy_agent_id == "a" ? 1 : 0) + tick * 100 + 13);
 
   // send each (alive) unit a random action
   for (const auto& unit_id: my_units)
   {
     const json& unit = game_state["unit_state"][unit_id];
+    //como o desafio só terá um inimigo, então só pego o primeiro inimigo
+    const json& enemy = game_state["unit_state"][enemy_units[0]];
+    if (enemy["hp"] != 0){
+    }
     if (unit["hp"] <= 0) continue;
+    //escolha aleatoria
     std::string action = _actions[rand() % _actions.size()];
     std::cout << "action: " << unit_id << " -> " << action << std::endl;
 
+    //envio do comando para o jogo
     if (action == "up" || action == "left" || action == "right" || action == "down")
     {
       GameState::send_move(action, unit_id);
